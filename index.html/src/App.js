973f3f483fc80e7b0de79173e7482945675ccdf9
xamyviii <dassamy08@gmail.com>
2023-12-28 20:28:45 +0530

    Firebase Added

--- index.html/src/App.js
+++ index.html/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>AMAZON</h1>
+      <h1>AMAZON CLONE</h1>
     </div>
   );
 }
---
+++ index.html/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA3cjFgI2GJM3okOXqF_MoTE_onJY3e4IE",
+    authDomain: "challenge-14bec.firebaseapp.com",
+    projectId: "challenge-14bec",
+    storageBucket: "challenge-14bec.appspot.com",
+    messagingSenderId: "209634288505",
+    appId: "1:209634288505:web:8c584f7135399980ccc5a8",
+    measurementId: "G-1ESRE4XX0R"
+  };
